9a45912917b902306e2fefc70d1a0175a9752901
Cooper <coopervk@gmail.com>
2020-08-30 00:16:46 -0500

    Base source file for Chapter 2 of book

--- Chapter2/Chapter2.cpp
+++ Chapter2/Chapter2.cpp
@@ -5,7 +5,6 @@ void Chapter2Unload(_In_ PDRIVER_OBJECT DriverObject);
 extern "C"
 NTSTATUS
 DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
-	UNREFERENCED_PARAMETER(DriverObject);
 	UNREFERENCED_PARAMETER(RegistryPath);
 
 	DriverObject->DriverUnload = Chapter2Unload;
