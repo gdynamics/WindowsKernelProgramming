48e55441a14262c548e90b17336089abf5a9ef70
Cooper <coopervk@gmail.com>
2020-09-08 23:10:21 -0500

    Add device & symlink to device init & destruct

--- Chapter4/Chapter4.cpp
+++ Chapter4/Chapter4.cpp
@@ -11,11 +11,28 @@ DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 	DriverObject->MajorFunction[IRP_MJ_CREATE] = PriorityBoosterCreateClose;
 	DriverObject->MajorFunction[IRP_MJ_CLOSE] = PriorityBoosterCreateClose;
 
+	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\PriorityBooster");
+	PDEVICE_OBJECT DeviceObject;
+	NTSTATUS status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create device object (0x%08X)\n", status));
+		return status;
+	}
+
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\PriorityBooster");
+	status = IoCreateSymbolicLink(&symLink, &devName);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
+		return status;
+	}
+
 	return STATUS_SUCCESS;
 }
 
 void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject) {
-	return;
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\PriorityBooster");
+	IoDeleteSymbolicLink(&symLink);
+	IoDeleteDevice(DriverObject->DeviceObject);
 }
 
 NTSTATUS PriorityBoosterCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp) {
