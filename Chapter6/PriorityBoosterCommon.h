e8ad301edae7fa33dd1e08e77a9a35cb89c543ba
Cooper <coopervk@gmail.com>
2021-02-10 22:22:04 -0600

    Add RAII management of heap pointers

--- Chapter6/PriorityBoosterCommon.h
+++ Chapter6/PriorityBoosterCommon.h
@@ -8,3 +8,24 @@ struct ThreadData {
 #define PRIORITY_BOOSTER_DEVICE 0x8000
 
 #define IOCTL_PRIORITY_BOOSTER_SET_PRIORITY CTL_CODE(PRIORITY_BOOSTER_DEVICE, 0x800, METHOD_NEITHER, FILE_ANY_ACCESS)
+
+template<typename T = void>
+class kunique_ptr {
+	T* _p;
+public:
+	kunique_ptr(T* p = nullptr) : _p(p) {}
+
+	~kunique_ptr() {
+		if (_p)
+			ExFreePool(_p);
+	}
+
+	T* operator->() const {
+		return _p;
+	}
+
+	T& operator*() const {
+		return *_p;
+
+	}
+};
